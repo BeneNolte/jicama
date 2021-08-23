0c8b30c3405b64648863582bddeac8a7b6ce853a
Lydia Seyler <lydia@inskip.fr>
2021-08-23 17:57:52 +0200

    saved

--- db/migrate/20210823155452_add_downloaded_to_datasources.rb
+++ db/migrate/20210823155452_add_downloaded_to_datasources.rb
@@ -1,5 +1,5 @@
 class AddDownloadedToDatasources < ActiveRecord::Migration[6.1]
   def change
-    add_column :datasources, :downloaded, :string
+    add_column :datasources, :downloaded, :boolean
   end
 end
