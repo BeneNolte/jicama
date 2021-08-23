0a9272e271e250d5522149cbf7ec8759b66d403e
Lydia Seyler <lydia@inskip.fr>
2021-08-23 17:56:16 +0200

    downloaded added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_23_151027) do
+ActiveRecord::Schema.define(version: 2021_08_23_155452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2021_08_23_151027) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "downloaded"
     t.index ["user_id"], name: "index_datasources_on_user_id"
   end
 
