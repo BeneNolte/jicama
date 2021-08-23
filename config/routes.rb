2d77ede57edeaea8db55c1c4201ccda04cd25402
Benedikt Nolte <bene_nolte@me.com>
2021-08-23 18:42:11 +0200

    Added routes and did not develop the dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  # get "dashboard", to: "pages#dashboard"
+  get "login", to: "pages#login"
+
+	resources :datasources, only: [ :show ] do
+		resources :locations, only: [ :index, :update ]
+		resources :data_ownerships, only: [ :index, :update ]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
