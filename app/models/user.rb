45bf10dfd5cf945739c541a9fcaf8e88b0d05dc4
Clemence Chalot <clemence.chalot@gmail.com>
2021-08-24 12:54:49 +0200

    Added login via Google
    Signed-off-by: Clemence Chalot <clemence.chalot@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  devise :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_many :datasources
   has_many :data_ownerships, through: :datasources
@@ -10,4 +11,43 @@ class User < ApplicationRecord
   has_many :youtube_channels, through: :datasources
   has_many :locations, through: :datasources
   has_many :interest_recommendations, through: :datasources
+
+
+  def self.find_for_oauth(auth)
+    # Create the user params
+    user_params = auth.slice("provider", "uid")
+    user_params.merge! auth.info.slice("email", "first_name", "last_name")
+    user_params[:picture_url] = auth.info.image
+    user_params[:token] = auth.credentials.token
+    user_params[:token_expiry] = Time.at(auth.credentials.expires_at)
+    user_params = user_params.to_h
+    # Finish creating the user params
+
+    # Find the user if there was a log in
+    user = User.find_by(provider: auth.provider, uid: auth.uid)
+
+    # If the User did a regular sign up in the past, find it
+    user ||= User.find_by(email: auth.info.email)
+
+    # If we had a user, update it
+    if user
+      user.update(user_params)
+    # Else, create a new user with the params that come from the app callback
+    else
+      user = User.new(user_params)
+      # create a fake password for validation
+      user.password = Devise.friendly_token[0,20]
+      user.save
+    end
+
+    return user
+  end
+
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+    user
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_23_155452) do
+ActiveRecord::Schema.define(version: 2021_08_24_092203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 2021_08_23_155452) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "downloaded"
+    t.boolean "downloaded"
     t.index ["user_id"], name: "index_datasources_on_user_id"
   end
 
@@ -83,6 +83,11 @@ ActiveRecord::Schema.define(version: 2021_08_23_155452) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "provider"
+    t.string "uid"
+    t.string "picture_url"
+    t.string "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
