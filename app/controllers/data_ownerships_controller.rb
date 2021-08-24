4d6d86d28889f3f698bc920443867fa959a90955
Benedikt Nolte <bene_nolte@me.com>
2021-08-24 17:13:05 +0200

    Added data_ownerships

--- app/controllers/data_ownerships_controller.rb
+++ app/controllers/data_ownerships_controller.rb
@@ -1,2 +1,18 @@
 class DataOwnershipsController < ApplicationController
+  def index
+    # @data_ownerships = DataOwnership.all
+    @data_ownerships = policy_scope(DataOwnership)
+    @datasource = Datasource.find(params[:datasource_id])
+  end
+
+  def edit
+    @data_ownership = DataOwnership.find(params[:id])
+  end
+
+  def update
+    @data_ownership = DataOwnership.find(params[:id])
+    authorize @data_ownership
+    @data_ownership.update(data_ownership_params)
+    redirect_to datasource_data_ownerships_path, notice: "Your data settings have been updated"
+  end
 end
