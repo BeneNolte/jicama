3e60920f5f9f4f775d603eea263aaa17cc677991
Lydia Seyler <lydia@inskip.fr>
2021-08-25 18:21:55 +0200

    to be tested

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,14 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
-  # get "dashboard", to: "pages#dashboard"
+  get "dashboard", to: "pages#dashboard"
   get "login", to: "pages#login"
+  get "loading", to: "pages#loading"
 
 	resources :datasources, only: [ :show ] do
 		resources :locations, only: [ :index, :update ]
 		resources :data_ownerships, only: [ :index, :update ]
   end
-
-
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
