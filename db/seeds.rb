556ce3e58f5f3318f281dec8919e4424a9fea41d
Clemence Chalot <clemence.chalot@gmail.com>
2021-08-26 18:58:51 +0200

    Created seeds of the user's data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,84 @@
+# CREATING THE SEEDS
+puts "Cleaning db"
+puts "🗑  Deleting all assets"
+
+User.destroy_all
+Datasource.destroy_all
+Location.destroy_all
+Company.destroy_all
+SearchHistory.destroy_all
+YoutubeHistory.destroy_all
+ChromeSearchWord.destroy_all
+ChromeVisitedLink.destroy_all
+YoutubeVideoTitle.destroy_all
+YoutubeVideoChannel.destroy_all
+
+
+puts 'Creating a user'
+user = User.new(email: "test@gmail.com", password: "123456", first_name: "Jicama", last_name: "Team")
+user.save!
+puts 'Finished!'
+
+puts 'Creating 5 datasources'
+instagram = Datasource.new(name: "Instagram", user: User.all.last, downloaded: false)
+instagram.save!
+spotify= Datasource.new(name: "Spotify", user: User.all.last, downloaded: false)
+spotify.save!
+twitter = Datasource.new(name: "Twitter", user: User.all.last, downloaded: false)
+twitter.save!
+facebook = Datasource.new(name: "Facebook", user: User.all.last, downloaded: false)
+facebook.save!
+google = Datasource.new(name: "Google", user: User.all.last, downloaded: true)
+google.save!
+file = URI.open('app/assets/images/Google.png')
+google.photo.attach(io: file, filename: 'Google.png', content_type: 'image/png')
+puts 'Finished!'
+
+puts 'Creating 5 locations...'
+boutique_orange_republique = Location.new(latitude: 488669322, longitude: 23635334, timestamp: "1517499133098", datasource: Datasource.all.last, status: true)
+boutique_orange_republique.save!
+rue_saintonge = Location.new(latitude: 488627548, longitude: 23636928, timestamp: "1517503380552", datasource: Datasource.all.last, status: true)
+rue_saintonge.save!
+mannerheim_gallery = Location.new(latitude: 488671050, longitude: 23617439, timestamp: "1517499133098", datasource: Datasource.all.last, status: true)
+mannerheim_gallery.save!
+opticien_grandoptical = Location.new(latitude: 488669636, longitude: 23634875, timestamp: "1517499133098", datasource: Datasource.all.last, status: true)
+opticien_grandoptical.save!
+season_market = Location.new(latitude: 488628811, longitude: 23620257, timestamp: "1517503380552", datasource: Datasource.all.last, status: true)
+season_market.save!
+puts 'Finished!'
+
+puts 'Creating 5 companies'
+axciom = Company.new(title: "axciom", url: "https://www.acxiom.com/")
+axciom.save!
+adform = Company.new(title: "adform", url: "https://site.adform.com/")
+adform.save!
+experian = Company.new(title: "experian", url: "https://www.experian.fr/")
+experian.save!
+levis = Company.new(title: "levis", url: "https://www.levi.com/")
+levis.save!
+apple = Company.new(title: "apple", url: "https://www.apple.com/")
+apple.save!
+puts 'Finished!'
+
+puts 'Creating data ownerships'
+own1 = DataOwnership.new(company: Company.order('RANDOM()').first, datasource: Datasource.all.last, status: true, type_of_ownership: "buyer")
+own1.save!
+own2 = DataOwnership.new(company: Company.order('RANDOM()').first, datasource: Datasource.all.last, status: true, type_of_ownership: "accessor")
+own2.save!
+own3 = DataOwnership.new(company: Company.order('RANDOM()').first, datasource: Datasource.all.last, status: false, type_of_ownership: "restricted")
+own3.save!
+own4 = DataOwnership.new(company: Company.order('RANDOM()').first, datasource: Datasource.all.last, status: false, type_of_ownership: "deleted")
+own4.save!
+puts 'Finished!'
+
+
+
 require 'json'
 require 'date'
 require 'nokogiri'
 descending = -1
 # TIPS: to find the right relative path use ====> Dir.pwd
+
 
 # PROFIL INFOS
 filepath = './db/TakeoutBene/Profile/Profile.json'
@@ -27,6 +103,17 @@ counts_monthly_search_words = Hash.new(0)
 monthlySearchWords.join(" ").split(" ").each { |word| counts_monthly_search_words[word] += 1 if word != "Google" && word != "Search" && word != "Untitled" && word != "Request" && word.length > 3 }
 rankedMonthlySearchWords = counts_monthly_search_words.sort_by { |key, value| value.to_i * descending}.to_a
 
+puts "Creating Chrome Search Words monthly seeds"
+rankedMonthlySearchWords.each do |element|
+  ChromeSearchWord.create(
+    word: element[0],
+    count: element[1],
+    time_range: "monthly",
+    datasource: google
+  )
+end
+puts "Finished!"
+
 # --> TOP SEARCH WORDS HISTORY OF ALL TIME
 allSearchWords = []
 browserHistories["Browser History"].each do |browserHistory|
@@ -36,6 +123,16 @@ counts_all_search_words = Hash.new(0)
 allSearchWords.join(" ").split(" ").each { |word| counts_all_search_words[word] += 1 if word != "Google" && word != "Search" && word != "Untitled" && word != "Request" && word.length > 3 }
 rankedAllSearchWords = counts_all_search_words.sort_by { |key, value| value.to_i * descending}.to_a
 
+puts "Creating Chrome Search Words from all time seeds"
+rankedAllSearchWords.each do |element|
+  ChromeSearchWord.create(
+    word: element[0],
+    count: element[1],
+    time_range: "all",
+    datasource: google
+  )
+end
+puts "Finished!"
 
 # 2. VISITED LINKS HISTORY
 # --> TOP VISITED LINKS OF CURRENT MONTH
@@ -48,6 +145,17 @@ browserHistories["Browser History"].each do |browserHistory|
 end
 rankedMonthlyVisitedLinks = monthlyVisitedLinks.group_by(&:itself).transform_values { |value| value.count }.sort_by { |_, value| value * descending}.to_a
 
+puts "Creating Chrome Visited Links monthly seeds"
+rankedMonthlyVisitedLinks.each do |element|
+  ChromeVisitedLink.create(
+    link: element[0],
+    count: element[1],
+    time_range: "monthly",
+    datasource: google
+  )
+end
+puts "Finished!"
+
 # --> TOP VISITED LINKS OF ALL TIME
 allVisitedLinks = []
 browserHistories["Browser History"].each do |browserHistory|
@@ -56,101 +164,66 @@ browserHistories["Browser History"].each do |browserHistory|
 end
 rankedAllVisitedLinks = allVisitedLinks.group_by(&:itself).transform_values { |value| value.count }.sort_by { |_, value| value * descending}.to_a
 
+puts "Creating Chrome Visited Links all time seeds"
+rankedAllVisitedLinks.each do |element|
+  ChromeVisitedLink.create(
+    link: element[0],
+    count: element[1],
+    time_range: "all",
+    datasource: google
+  )
+end
+puts "Finished!"
 
 # 3. YOUTUBE CHANNEL HISTORY
 html_file = File.open('./db/TakeoutBene/YouTube and YouTube Music/history/watch-history.html')
 html_doc = Nokogiri::HTML(html_file)
 
 # --> TOP VIDEO TITLES OF ALL TIMES
-videoTitles = []
+videos = []
 videoTitlesExtract = html_doc.css("div.mdl-grid div:nth-child(2) :first-child")
 # trying to retrieve the date : p videoTitlesExtract.first.text
 videoTitlesExtract.each do |element|
-  if element.text.length > 3
-    videoTitles << element.text.gsub(/[^[:ascii:]]/, "").encode("iso-8859-1").force_encoding("utf-8")
+  if element.text.length > 3 && element.attribute('href')&.value.present?
+    videos << [element.text.gsub(/[^[:ascii:]]/, "").encode("iso-8859-1").force_encoding("utf-8"), element.attribute('href').value]
   end
 end
-rankedVideoTitles = videoTitles.group_by(&:itself).transform_values { |value| value.count }.sort_by { |_, value| value * descending}.to_a
+rankedVideoTitles = videos.group_by(&:itself).transform_values { |value| value.count }.sort_by { |_, value| value * descending}.to_a
+
+puts "Creating Youtube Video Titles all time seeds"
+rankedVideoTitles.each do |element|
+  YoutubeVideoTitle.create(
+    title: element[0][0],
+    count: element[1],
+    url: element[0][1],
+    time_range: "all",
+    datasource: google
+  )
+end
+puts "Finished!"
+
 # --> TOP CHANNELS FROM ALL TIMES
-videoChannels = []
+channels = []
 videoChannelsExtract = html_doc.css("div.mdl-grid div:nth-child(2) a")
 videoChannelsExtract.each do |element|
-  if element.text.length > 3
-    videoChannels << element.text.gsub(/[^[:ascii:]]/, "").encode("iso-8859-1").force_encoding("utf-8")
+  if element.text.length > 3 && element.attribute('href')&.value.present?
+    channels << [element.text.gsub(/[^[:ascii:]]/, "").encode("iso-8859-1").force_encoding("utf-8"), element.attribute('href').value]
   end
 end
-rankedVideoChannels = videoChannels.group_by(&:itself).transform_values { |value| value.count }.sort_by { |_, value| value * descending}.to_a
+rankedVideoChannels = channels.group_by(&:itself).transform_values { |value| value.count }.sort_by { |_, value| value * descending}.to_a
 
-# Links if we want them ? channelLinks = html_doc.css("div.mdl-grid div:nth-child(2) a").attribute('href').value
+puts "Creating Youtube Video Channels all time seeds"
+rankedVideoChannels.each do |element|
+  YoutubeVideoChannel.create(
+    title: element[0][0],
+    count: element[1],
+    url: element[0][1],
+    time_range: "all",
+    datasource: google
+  )
+end
+puts "Finished!"
 
-
-# CREATING THE SEEDS
-puts "Cleaning db"
-puts "🗑  Deleting all assets"
-
-User.destroy_all
-Datasource.destroy_all
-Location.destroy_all
-Company.destroy_all
-SearchHistory.destroy_all
-YoutubeHistory.destroy_all
-
-puts 'Creating a user'
-user = User.new(email: "test@gmail.com", password: "123456", first_name: "Jicama", last_name: "Team")
-user.save!
-puts 'Finished user'
-
-puts 'Creating 5 datasources'
-instagram = Datasource.new(name: "Instagram", user: User.all.last, downloaded: false)
-instagram.save!
-spotify= Datasource.new(name: "Spotify", user: User.all.last, downloaded: false)
-spotify.save!
-twitter = Datasource.new(name: "Twitter", user: User.all.last, downloaded: false)
-twitter.save!
-facebook = Datasource.new(name: "Facebook", user: User.all.last, downloaded: false)
-facebook.save!
-google = Datasource.new(name: "Google", user: User.all.last, downloaded: true)
-google.save!
-file = URI.open('app/assets/images/Google.png')
-google.photo.attach(io: file, filename: 'Google.png', content_type: 'image/png')
-puts 'Finished 5 datasources'
-
-puts 'Creating 5 locations...'
-boutique_orange_republique = Location.new(latitude: 488669322, longitude: 23635334, timestamp: "1517499133098", datasource: Datasource.all.last, status: true)
-boutique_orange_republique.save!
-rue_saintonge = Location.new(latitude: 488627548, longitude: 23636928, timestamp: "1517503380552", datasource: Datasource.all.last, status: true)
-rue_saintonge.save!
-mannerheim_gallery = Location.new(latitude: 488671050, longitude: 23617439, timestamp: "1517499133098", datasource: Datasource.all.last, status: true)
-mannerheim_gallery.save!
-opticien_grandoptical = Location.new(latitude: 488669636, longitude: 23634875, timestamp: "1517499133098", datasource: Datasource.all.last, status: true)
-opticien_grandoptical.save!
-season_market = Location.new(latitude: 488628811, longitude: 23620257, timestamp: "1517503380552", datasource: Datasource.all.last, status: true)
-season_market.save!
-puts 'Finished 5 locations'
-
-puts 'Creating 5 companies'
-axciom = Company.new(title: "axciom", url: "https://www.acxiom.com/")
-axciom.save!
-adform = Company.new(title: "adform", url: "https://site.adform.com/")
-adform.save!
-experian = Company.new(title: "experian", url: "https://www.experian.fr/")
-experian.save!
-levis = Company.new(title: "levis", url: "https://www.levi.com/")
-levis.save!
-apple = Company.new(title: "apple", url: "https://www.apple.com/")
-apple.save!
-puts 'Finished 5 companies'
-
-puts 'Creating data ownerships'
-own1 = DataOwnership.new(company: Company.order('RANDOM()').first, datasource: Datasource.all.last, status: true, type_of_ownership: "buyer")
-own1.save!
-own2 = DataOwnership.new(company: Company.order('RANDOM()').first, datasource: Datasource.all.last, status: true, type_of_ownership: "accessor")
-own2.save!
-own3 = DataOwnership.new(company: Company.order('RANDOM()').first, datasource: Datasource.all.last, status: false, type_of_ownership: "restricted")
-own3.save!
-own4 = DataOwnership.new(company: Company.order('RANDOM()').first, datasource: Datasource.all.last, status: false, type_of_ownership: "deleted")
-own4.save!
-puts 'Finished data ownerships'
 
 puts "Creating Bene's search history"
 beneSearchHistory = SearchHistory.create(
@@ -162,7 +235,7 @@ beneSearchHistory = SearchHistory.create(
   deleted: false,
   datasource: google
 )
-puts "Finsih creating Bene's search history"
+puts "Finsihed!"
 
 puts "Creating Bene's youtube history"
 beneYoutubeHistory = YoutubeHistory.create(
@@ -172,4 +245,4 @@ beneYoutubeHistory = YoutubeHistory.create(
   deleted: false,
   datasource: google
 )
-puts "Finsih creating Bene's youtube history"
+puts "Finsihed!"
