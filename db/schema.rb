96c271c703f7de735d25622ff6194789fa6214da
Benedikt Nolte <bene_nolte@me.com>
2021-08-26 14:44:29 +0200

    Privacy score

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_26_092033) do
+ActiveRecord::Schema.define(version: 2021_08_26_110049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,8 @@ ActiveRecord::Schema.define(version: 2021_08_26_092033) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "downloaded"
+    t.float "size"
+    t.float "score"
     t.index ["user_id"], name: "index_datasources_on_user_id"
   end
 
