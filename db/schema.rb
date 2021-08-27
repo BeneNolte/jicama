99a7f0ff6c9a3bb37309374ad6a0281c8a870f49
Clemence Chalot <clemence.chalot@gmail.com>
2021-08-27 12:15:03 +0200

    Drop useless tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_26_161942) do
+ActiveRecord::Schema.define(version: 2021_08_27_101409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
