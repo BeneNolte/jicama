5e0f02fcde785163da918a97c01cab102c0d2827
Pierre Grandjean <pierrebigjean@gmail.com>
2021-09-02 20:05:46 +0200

    wait

--- app/javascript/pages/home.js
+++ app/javascript/pages/home.js
@@ -1,6 +1,9 @@
 const logoHome = () => {
   const bigLogo = document.querySelector(".logo-sizing")
   const smallLogo = document.querySelector(".logo-navbar")
+  
+  const homeButton = document.getElementById("main-home-button")
+
 
   if (bigLogo) {
     window.addEventListener('scroll', () => {
@@ -10,6 +13,24 @@ const logoHome = () => {
       smallLogo.style.opacity = 1 - 1/((window.scrollY + 50)/50);
 
     })
+    
+    let secs = 10 * 60;
+    let count = 0;
+    setInterval(() => {
+      var $badge = $('#nhb_01');
+      count += 500;
+      $badge.text(count.toFixed(2));
+    }, secs);
+    console.log(count)
+    
+    homeButton.addEventListener("click", (event) => {
+
+      event.preventDefault();
+      setTimeout(() => {}, 1000);
+      homeButton.style.boxShadow = `0px 0px 32px ${count}px white`; 
+    })
+    
   }
+
 }
 export { logoHome };
