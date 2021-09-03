17534f888a78e9cc349346145a1aa51b99b6dbb1
Benedikt Nolte <bene_nolte@me.com>
2021-09-03 13:06:34 +0200

    fixed access keys

--- app/jobs/data_parse_job.rb
+++ app/jobs/data_parse_job.rb
@@ -1,25 +1,25 @@
-require 'zip'
+# require 'zip'
 
-class DataParseJob < ApplicationJob
-  queue_as :default
+# class DataParseJob < ApplicationJob
+#   queue_as :default
 
-  def perform(datasource)
-    # Do something later
+#   def perform(datasource)
+#     # Do something later
 
-    # Download file
-    # zip = amazon.bucket('jicama').object("#{@datasource.file.path}").get(response_target: "#{@datasource.file.url}")
-    url = datasource.file.url
-    zip = URI.open(url)
-    # unzip file
-    Zip::File.open(zip) do |zipfile|
-      zipfile.each do |file|
-        # do something with file
+#     # Download file
+#     # zip = amazon.bucket('jicama').object("#{@datasource.file.path}").get(response_target: "#{@datasource.file.url}")
+#     url = datasource.file.url
+#     zip = URI.open(url)
+#     # unzip file
+#     Zip::File.open(zip) do |zipfile|
+#       zipfile.each do |file|
+#         # do something with file
 
-        p file.name == "__MACOSX/TakeoutBene/Location History/._Location History.json"
-      end
-    end
-    # Select relevant folders
-    # Stock relevant folder in database
-    puts "OK I'm done now"
-  end
-end
+#         p file.name == "__MACOSX/TakeoutBene/Location History/._Location History.json"
+#       end
+#     end
+#     # Select relevant folders
+#     # Stock relevant folder in database
+#     puts "OK I'm done now"
+#   end
+# end
